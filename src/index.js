d53533465d726db2c9c40940a094e09f2732a3c6
Stephanie912-lab <stephanieamondi@gmail.com>
2024-12-14 00:01:12 +0300

    added the interactive part of javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("hello)")
+function generatePoem(event){
+    event.preventDefault();
+
+    new Typewriter ("#poem",{
+        strings:"la tombe dit a la rose",
+        autostart:true,
+        delay: 1,
+        cursor:"",
+    });
+}
+
+let poemElement=document.querySelector("#poem-generate-form");
+poemElement.addEventListener("submit",generatePoem);
